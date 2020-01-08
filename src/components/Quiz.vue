7a76509411381bcc1e43704230afa3e9cd8728f3
Trey <trey@lavidadev.com>
2020-01-07 19:23:24 -0500

    Restructure Project

---
+++ src/components/Quiz.vue
@@ -0,0 +1,23 @@
+<template>
+    <section>
+        <pre>{{ JSON.stringify(questions, null, 2) }}</pre>
+    </section>
+</template>
+
+<script>
+export default {
+  props: {
+    questions: {
+      type: Array,
+      required: true
+    }
+  },
+  data() {
+    return {};
+  }
+};
+</script>
+
+<style>
+
+</style>
---
+++ src/pages/home/index.vue
@@ -0,0 +1,50 @@
+<template>
+<div>
+  <article>
+    <Quiz v-bind:questions="questions"/>
+  </article>
+</div>
+</template>
+
+<script>
+import Quiz from '../../components/Quiz';
+export default {
+  props: {
+    questions: {
+      type: Array,
+      required: true
+    }
+  },
+  components: {
+    Quiz
+  },
+  data() {
+    return {};
+  }
+};
+</script>
+
+<style>
+body {
+  padding: 0;
+  margin: 0;
+}
+
+div {
+  width: 100vw;
+  height: 100vh;
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  align-content: center;
+  align-items: center;
+}
+
+article {
+  width: 50vw;
+  height: 50vh;
+  max-height: 50vh;
+  overflow: scroll;
+  border: 1px solid #000;
+}
+</style>
